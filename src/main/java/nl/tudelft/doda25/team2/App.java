b295e84ac67d1a190aee3fc2234b073f272b41fe
Nick van Luijk <N.vanLuijk@student.tudelft.nl>
2025-11-18 11:34:14 +0100

    Add empty project

---
+++ src/main/java/nl/tudelft/doda25/team2/App.java
@@ -0,0 +1,10 @@
+package nl.tudelft.doda25.team2;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
